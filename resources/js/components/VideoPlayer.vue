049e101966b9a60f7eeaaea8e4fcfc8961e12861
John Puddephatt <john@jdp.org.uk>
2021-09-25 21:53:22 +0100

    change video type within lessons

--- resources/js/components/VideoPlayer.vue
+++ resources/js/components/VideoPlayer.vue
@@ -38,8 +38,8 @@ export default {
   },
 
   watch: {
-    should_autoplay: function(val) {
-      if (val && !this.isLoading) {
+    should_autoplay: function(autoplay) {
+      if (autoplay && !this.isLoading) {
         this.player.play();
       } else {
         this.player.pause();
@@ -51,6 +51,9 @@ export default {
   mounted() {
     this.player = videojs(this.$refs.player, {});
 
+    this.player.options.hls.overrideNative = true;
+    this.player.options.html5.nativeAudioTracks = false;
+    this.player.options.html5.nativeVideoTracks = false;
     this.player.on("ended", this.onEnded);
 
     this.player.on("play", this.onPlay);
--- resources/views/editorjs/video.blade.php
+++ resources/views/editorjs/video.blade.php
@@ -1,8 +1,9 @@
 <div class="editor-js-block editor-js-block__video">
   <figure>
-    <video-player ratio="widescreen" source="{!! $data['file']['playlist'] !!}" type="application/x-mpegURL"></video-player>
-  @if($data['caption'])
+    <video-player ratio="widescreen" source="{!! $data['file']['playlist'] !!}" type="application/vnd.apple.mpegURL">
+    </video-player>
+    @if($data['caption'])
     <figcaption class="editor-js-video--caption">{{ $data['caption'] }}</figcaption>
-  @endif
+    @endif
   </figure>
 </div>
